4c72d7057da986dd6f7ed9603882befa9d6a13a9
skyfinging <250805603@qq.com>
2020-04-08 22:42:12 +0800

    java内存溢出示例

--- demo-out-of-memory/src/main/java/demo/MainJHS.java
+++ demo-out-of-memory/src/main/java/demo/MainJHS.java
@@ -1,8 +1,19 @@
 package demo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class MainJHS {
     // -Xmx10m -Xms10m
     public static void main(String[] args) {
-        byte[] bytes = new byte[50 * 1024 * 1024];
+//        byte[] bytes = new byte[50 * 1024 * 1024];
+        String str = "";
+        List<String> list = new ArrayList<>();
+        while(true){
+            str += "aaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            list.add(str.intern());
+        }
+
     }
 }
